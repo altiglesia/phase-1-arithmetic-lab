9334d9d1b4e60f82751b7d6117bd067b05c765f2
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-18 21:24:45 -0400

    commit tree aa23dc860e067c03206bc73775e663333bd10179

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 1
+Number.isInteger(random);
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = 20
+Math.max(1, 2, 3, 4, 20);
